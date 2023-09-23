1b89ed12d0077085fd52a7b17ae5c1b465765040
Oleg Alexander <olegalexander@gmail.com>
2023-09-22 17:35:34 -0700

    Finished text processing example.

--- intro_to_fp_bird_wadler/ch4/text_processing.py
+++ intro_to_fp_bird_wadler/ch4/text_processing.py
@@ -1,7 +1,7 @@
 from typing import NewType
 from itertools import chain, islice, pairwise
-from more_itertools import split_at, intersperse, flatten
-from intro_to_fp_bird_wadler.common import inits
+from more_itertools import split_at, intersperse, flatten, take
+from intro_to_fp_bird_wadler.common import inits, drop
 
 Line = NewType('Line', str)
 Word = NewType('Word', str)
@@ -66,7 +66,52 @@ def normalize(text: str) -> str:
     return unparse(parse(text))
 
 
+def greedy(line_length: int, words: list[Word]) -> int:
+    """Greedy algorithm for line breaking."""
+    return max([len(us) for us in inits(words) if len(unwords(us)) <= line_length])
+
+
+def fill(line_length: int, words: list[Word]) -> list[list[Word]]:
+    if len(words) == 0:
+        return []
+    else:
+        n = greedy(line_length, words)
+        first_line = take(n, words)
+        rest_words = drop(n, words)
+        return [first_line] + fill(line_length, rest_words)
+
+
+def linewords(lines: list[Line]) -> list[Word]:
+    return list(flatten(map(words, lines)))
+
+
+def textparas(text: str) -> list[list[Word]]:
+    return list(map(linewords, paras(lines(text))))
+
+
+def filltext(line_length: int, text: str) -> str:
+    return unparse(list(map(lambda words: fill(line_length, words), textparas(text))))
+
+
 if __name__ == "__main__":
+
+    assert filltext(72, """Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ac facilisis lorem. Pellentesque ullamcorper posuere nulla, quis rutrum mi. Mauris nec massa imperdiet, porttitor leo ut, venenatis enim. Aliquam ullamcorper, enim et imperdiet pharetra, purus mauris semper lacus, vitae condimentum orci nisl bibendum mauris. Pellentesque eleifend tellus sit amet dui mollis, a dignissim lacus ultrices.\n\nSuspendisse in massa nec nisi cursus egestas. Nam justo arcu, imperdiet ac lorem a, lobortis aliquam purus. Duis eu erat at lacus consequat posuere eu at elit. Ut eget urna eu lacus fermentum rhoncus. Donec ultrices erat id eros convallis consequat. Vivamus ut maximus eros. Etiam quis sapien aliquam, molestie dolor tincidunt, lacinia ipsum. Suspendisse at aliquam purus. Interdum et malesuada fames ac ante ipsum primis in faucibus. Integer aliquet lacus purus, quis dapibus lorem elementum id. Nunc ut lacus nec nisi fermentum posuere vel sit amet nunc.""") == """Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ac
+facilisis lorem. Pellentesque ullamcorper posuere nulla, quis rutrum mi.
+Mauris nec massa imperdiet, porttitor leo ut, venenatis enim. Aliquam
+ullamcorper, enim et imperdiet pharetra, purus mauris semper lacus,
+vitae condimentum orci nisl bibendum mauris. Pellentesque eleifend
+tellus sit amet dui mollis, a dignissim lacus ultrices.
+
+Suspendisse in massa nec nisi cursus egestas. Nam justo arcu, imperdiet
+ac lorem a, lobortis aliquam purus. Duis eu erat at lacus consequat
+posuere eu at elit. Ut eget urna eu lacus fermentum rhoncus. Donec
+ultrices erat id eros convallis consequat. Vivamus ut maximus eros.
+Etiam quis sapien aliquam, molestie dolor tincidunt, lacinia ipsum.
+Suspendisse at aliquam purus. Interdum et malesuada fames ac ante ipsum
+primis in faucibus. Integer aliquet lacus purus, quis dapibus lorem
+elementum id. Nunc ut lacus nec nisi fermentum posuere vel sit amet
+nunc."""
+
     assert normalize("Line   one.\nLine    two.\n\n\nLine   three.") == """Line one.
 Line two.
 
